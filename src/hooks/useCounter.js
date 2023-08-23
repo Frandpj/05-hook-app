6d6a42498f30538361f20bf83688c1cd8846474b
Fran_dpj <fdelpinojimenez@gmail.com>
2023-08-23 09:21:38 +0200

    Pruebas useCounter

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -5,12 +5,12 @@ export const useCounter = (initialValue = 10) => {
     const [counter, setCounter] = useState(initialValue);
 
     const increment = (value = 1) => {
-        setCounter(counter + value);
+        setCounter( (current) => current + value);
     }
 
     const decrement = (value = 1) => {
-        if (counter === 0) return;
-        setCounter(counter - value);
+        // if (counter === 0) return;
+        setCounter((current) => current - value);
     }
 
     const reset = () => {
